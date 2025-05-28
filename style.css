e297d0ceb084d25aa6ec081bcdfe88d28257abbf
Ellen Luiza <137297820+yukieheaven@users.noreply.github.com>
2025-05-28 12:26:39 -0300

    html

---
+++ style.css
@@ -0,0 +1,36 @@
+:root {
+  --cor-fundo: #01080E;
+  --cor-principal: #0B0D20;
+  --cor-secundaria: #212333;
+  --cor-destaque: #2BDEFD;
+  --cor-texto: #D7F9FF;
+}
+body {
+  background-color: var(--cor-fundo);
+  color: var(--cor-texto);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+.caixa-principal {
+  background-color: var(--cor-principal);
+  width: 90%;
+  text-align: center;
+  padding: 20px;
+}
+h1 {
+  color: var(--cor-destaque);
+}
+button {
+  background-color: var(--cor-principal);
+  color: var(--cor-destaque);
+  border: none;
+  border-radius: 15px;
+  padding: 15px;
+  transition: background-color 0.3s;
+}
+button:hover {
+  background-color: var(--cor-destaque);
+  color: var(--cor-secundaria);
+}
